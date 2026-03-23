0c74eee10913365386e0a1e8a0cab2b0fb614368
Joyo-G <gustavo.joyo.p@gmail.com>
2026-03-22 21:02:01 -0300

    refactor: switch hyperlinked serializer for model serializer

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,17 +1,17 @@
 from .models import Course, AcademicPeriod, CourseOffering, Section, Schedule
 from rest_framework import serializers
 
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['code', 'name', 'description', 'credits', 'tags']
 
-class AcademicPeriodSerializer(serializers.HyperlinkedModelSerializer):
+class AcademicPeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = AcademicPeriod
         fields = ['name', 'term', 'year']
 
-class CourseOfferingSerializer(serializers.HyperlinkedModelSerializer):
+class CourseOfferingSerializer(serializers.ModelSerializer):
     course = CourseSerializer(read_only=True)
     academic_period = AcademicPeriodSerializer(read_only=True)
 
@@ -19,14 +19,14 @@ class CourseOfferingSerializer(serializers.HyperlinkedModelSerializer):
         model = CourseOffering
         fields = ['course', 'academic_period', 'offering_comment', 'type']
 
-class SectionSerializer(serializers.HyperlinkedModelSerializer):
+class SectionSerializer(serializers.ModelSerializer):
     course_offering = CourseOfferingSerializer(read_only=True)
 
     class Meta:
         model = Section
         fields = ['course_offering', 'section_number', 'mobility_quota', 'service_quota', 'control_count', 'has_exam', 'has_survey', 'quota', 'modality', 'management_comment']
 
-class ScheduleSerializer(serializers.HyperlinkedModelSerializer):
+class ScheduleSerializer(serializers.ModelSerializer):
     section = SectionSerializer(read_only=True)
 
     class Meta:
